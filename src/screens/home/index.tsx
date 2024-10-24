799d6901ab6422aef6342b1ad41fb8f8cec6da60
Harsh Dadhich <harshdadhich1109@gmail.com>
2024-10-24 12:56:59 +0530

    axios change

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -149,7 +149,7 @@ export default function Home() {
             const response = await axios({
                 method: 'post',
                 // url: `${import.meta.env.VITE_API_URL}/calculate`,
-                url: `http://localhost:8900/calculate`,
+                url: `https://dadhich-ai-calc-be.vercel.app/calculate`,
                 data: {
                     image: canvas.toDataURL('image/png'),
                     dict_of_vars: dictOfVars
